663d312624b1e694bd08ab3db0b226fe1842d94b
Daniel Soney <daniel.m.soney@student.uts.edu.au>
2026-04-14 18:27:41 +1000

    Initial Spring Boot project and Postgres setup

---
+++ src/main/java/com/nba/nba_arena/NbaArenaApplication.java
@@ -0,0 +1,13 @@
+package com.nba.nba_arena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaArenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nba/nba_arena/NbaArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nba.nba_arena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaArenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
